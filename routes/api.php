0af2a357f34f4a7a59f231a2157ce51e7d84c716
zhaiyuxin103 <zhaiyuxin103@hotmail.com>
2023-10-16 07:50:07 +0800

    perf(api): 频率限制

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('throttle:'.config('api.rate_limits.sign'))
+    ->group(function () {
+
+    });
+
+Route::middleware('throttle:'.config('api.rate_limits.access'))
+    ->group(function () {
+
+    });
