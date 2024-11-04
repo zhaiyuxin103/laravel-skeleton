a27e30acdced903401145b0a3c214e33369868b4
zhaiyuxin103 <zhaiyuxin103@hotmail.com>
2024-11-04 08:45:12 +0800

    build(deps): add daisyui

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,14 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
-    { languageOptions: { globals: globals.browser } },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+    },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     {
