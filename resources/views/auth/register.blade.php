22c029b745e8dce01967acbe4f6796ff464daaa8
zhaiyuxin103 <zhaiyuxin103@hotmail.com>
2025-01-18 13:54:31 +0800

    perf(auth): 调整注册页面中利用規約和プライバシーポリシー的链接

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -130,8 +130,8 @@
                             <div class="ms-2">
                                 {!!
                                     __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="' . route('terms.show') . '" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' . __('Terms of Service') . '</a>',
-                                        'privacy_policy' => '<a target="_blank" href="' . route('policy.show') . '" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' . __('Privacy Policy') . '</a>',
+                                        'terms_of_service' => '<a target="_blank" href="' . route('terms') . '" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' . __('Terms of Service') . '</a>',
+                                        'privacy_policy' => '<a target="_blank" href="' . route('privacies') . '" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' . __('Privacy Policy') . '</a>',
                                     ])
                                 !!}
                             </div>
