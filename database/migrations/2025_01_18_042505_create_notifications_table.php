79aeae277d6ea8b604ee5aea38d723c782ea62a4
zhaiyuxin103 <zhaiyuxin103@hotmail.com>
2025-01-18 12:28:48 +0800

    chore(notifications): migration file

---
+++ database/migrations/2025_01_18_042505_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
+            $table->boolean('state')->default(true)->comment('状态');
+            $table->unsignedInteger('sort')->default(0)->comment('排序');
+            $table->timestampsTz();
+            $table->softDeletesTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
