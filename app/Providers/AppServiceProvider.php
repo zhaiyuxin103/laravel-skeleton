a0ff3c7d304c90a539ed4954b473703862fcacaf
YuXin Zhai <zhaiyuxin103@gmail.com>
2025-12-13 19:19:51 +0800

    chore: 添加 APP_HTTPS 环境变量

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.https')) {
+            URL::forceScheme('https');
+        }
     }
 }
--- config/app.php
+++ config/app.php
@@ -125,4 +125,6 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'https' => env('APP_HTTPS', false),
+
 ];
